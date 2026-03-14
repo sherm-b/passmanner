21491933fc79e22b3359e6a92768d81c53d338ea
Jeremy B <jeremy.steven.bess@gmail.com>
2026-03-13 20:08:43 -0700

    start building main user input loop, add help menu

--- main.py
+++ main.py
@@ -20,13 +20,21 @@ def create_vault():
             with open('pmvault.csv', 'w', newline='') as vault_file:
                 vault_writer = csv.writer(vault_file)
                 vault_writer.writerow(['#', 'account_name', 'username', 'password'])
-                print(f"Vault created at {os.getcwd()}\\pmvault.csv")
+                print(f"Vault created at {os.getcwd()}\\pmvault.csv\n")
                 input_invalid = False
         elif create_vault_input.strip().lower() == 'n':
             sys.exit("passmanner needs a vault file to function. Exiting...")
         else:
             create_vault_input = input("Please enter a valid input. [y/n] ")
 
+def print_help():
+    """Prints the help menu"""
+    print("[a] - add - Add account details to passmanner vault.\n"
+          "[l] - list - List account details in passmanner vault.\n"
+          "[s] - search - Search for account details in passmanner vault by account type or username.\n"
+          "[d] - delete - Delete account details from passmanner vault.\n"
+          "[q] - quit - Quit passmanner.\n"
+          "[h] - help - Show this help menu.\n")
 
 def main():
     print("-- passmanner --\n")
@@ -35,6 +43,12 @@ def main():
         create_vault()
     #ask if user wants to search for, see, add or delete profile info, accept input
     #conditional tree to selected option, loops if not terminated
+    while True:
+        input_choice = input("Would you like to [a]dd, [l]ist, [s]earch for, or [d]elete an account?\n"
+                             "You can enter 'h' for help, or 'q' to quit.\n"
+                             "[a/l/s/d/q/h]: ").strip().lower()
+        if input_choice == 'h':
+            print_help()
 
 if __name__ == '__main__':
     main()
