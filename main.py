826eb819701dc7f3369c38e3471bec85716619f4
Jeremy B <jeremy.steven.bess@gmail.com>
2026-03-13 18:07:00 -0700

    add header, rough sketch of functionality for organization

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
-def main():
+###################################
+#Passmanner By Jeremy Bess
+#Final Project for CS162P
+#3/13/2026
+###################################
+import passmanner
 
+def main():
+    #if vault file doesn't exist, create it
+    print("""-- passmanner --""")
+    #ask if user wants to search for, see, add or delete profile info, accept input
+    #conditional tree to selected option, loops if not terminated
 
 if __name__ == '__main__':
     main()
